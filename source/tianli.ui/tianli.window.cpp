fbac6330bd226fa0d5e76a65a5ea055088c3bce6
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-09-18 21:19:27 +0800

    feat:设置无边框透明窗口

--- source/tianli.ui/tianli.window.cpp
+++ source/tianli.ui/tianli.window.cpp
@@ -7,9 +7,9 @@ TianLiWindow::TianLiWindow(QWidget *parent) : QWidget(parent)
 {
     ui.setupUi(this);
     // 设置窗口透明
-    // setAttribute(Qt::WA_TranslucentBackground);
+    setAttribute(Qt::WA_TranslucentBackground);
     // 设置窗口无边框
-    // setWindowFlags(Qt::FramelessWindowHint);
+    setWindowFlags(Qt::FramelessWindowHint);
 
     // 自动继承MapView
     // MapView *mapView = new MapView(ui.widget);
